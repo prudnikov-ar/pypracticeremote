fbb0a0533489872bc05d71c371f1a000debda642
prudnikov_ar <prudnikov_ar@icloud.com>
2024-07-01 18:43:59 +0700

    openCV тест камеры

--- test.py
+++ test.py
@@ -1,6 +1,33 @@
-# import torch
-# from torch import nn
-# from torch.utils.data import DataLoader
-# from torchvision import datasets
-# from torchvision.transforms import ToTensor
-# print("lesssssgo")
+import cv2
+
+def blure_face(img):
+    (h, w) = img.shape[:2]
+    dW = int(w/3.0)
+    dH = int(h/3.0)
+    if dW % 2 == 0:
+        dW -= 1
+    if dH % 2 == 0:
+        dH -= 1
+    return cv2.GaussianBlur(img, (dW, dH), 0)
+
+capture = cv2.VideoCapture(0)
+face = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+while True:
+    ret, img = capture.read()
+
+    faces = face.detectMultiScale(img, scaleFactor=1.5, minNeighbors=5, minSize=(20, 20))
+
+
+    for (x, y, w, h) in faces:
+        # cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        img[y:y+h, x:x+w] = blure_face(img[y:y+h, x:x+w])
+
+    cv2.imshow("Andrew", img)
+
+    k = cv2.waitKey(30) & 0xFF
+    if k == 27:
+        break
+
+capture.release()
+cv2.destroyAllWindows()
